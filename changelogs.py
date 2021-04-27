8e9cf5526fb69a0b76398e9101688967091d6d0d
Jason Barrie Morley <jbmorley@mac.com>
2021-04-26 22:43:08 -0700

    feat: Basic support for synthesizing a version from a set of commits

--- changelogs.py
+++ changelogs.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
+import re
 import subprocess
-
 
 
 class Version(object):
@@ -24,13 +24,38 @@ class Version(object):
     def bump_patch(self):
         self.patch = self.patch + 1
 
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.patch}"
+
 
 def main():
     parser = argparse.ArgumentParser()
     options = parser.parse_args()
 
-    logs = subprocess.check_output(["git", "log", "--pretty=format:%s"]).decode("utf-8").split("\n")
-    print(logs)
+    version = Version()
+
+    parser = re.compile(r"^(.+?):.+")
+
+    operations = {
+        "BREAKING CHANGE": lambda x: x.bump_major(),
+        "feat": lambda x: x.bump_minor(),
+        "fix": lambda x: x.bump_patch(),
+    }
+
+    logs = reversed(subprocess.check_output(["git", "log", "--pretty=format:%s"]).decode("utf-8").split("\n"))
+    for log in logs:
+        result = parser.match(log)
+        if result:
+            commit_type = result.group(1)
+            operation = operations[commit_type]
+            operation(version)
+            print(f"{version} ({commit_type})")
+        else:
+            print("Not sure what to do with this log line?")
+        print(log)
+
+
+
 
 
 
